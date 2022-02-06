1f31123b6bc54a126888080e6e88d5f87bdaf953
Favour chukwuedo <fchukwuedo@gmail.com>
2022-02-06 22:33:24 +0100

    contracts mumbai and edits

--- packages/frontend/artifacts/contracts/contractAddress.ts
+++ packages/frontend/artifacts/contracts/contractAddress.ts
@@ -1,2 +1,2 @@
-export const CheesecakePortalContract = '0x72501A5F53e66937E90747B20bc0178b4b0eB42F'
-export const MulticallContract = '0xa5C4Dc203E6109034D446C82e22913FaDE20b331'
+export const CheesecakePortalContract = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+export const MulticallContract = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
